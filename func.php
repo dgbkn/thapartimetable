c8a139caa3993f559e1083fe9c5cd59177fb5ca4
dgbkn <65810521+dgbkn@users.noreply.github.com>
2023-07-30 08:45:42 +0000

    added fuc** time parser

--- func.php
+++ func.php
@@ -1,216 +1,196 @@
 <?php
 
-function getMagnificentKeys($arr){
-  $keys = [];
-  $prev = 0;
-  foreach($arr as $k => $v){
-     if(abs($prev - $v) > 7){   
-        // $keys[] = $k;
-       $keys[$k] = "break";
-     }else{
-     $prev = $v;
-     $keys[$k] = "batchdet";
-
-     }
-  }
-return $keys;
-}
-
-
-
-function addArrays($a,$b){
-  foreach($a as $k=>$v){
-    $vv = $b[$k];
-    if(!empty($v) && !empty($vv)){
-      $a[$k] = $v . "," .$vv; 
-    }
-  }
-  return $a;
-}
-
-
-
-function checkLec($s){
-  if (checkSubjectByRegex($s)){
-   $words = explode(' ', $s);
-   $firstWord = $words[0];
-    if(substr($firstWord, -1) == "L"){
-      return true;
-    }
-   $secWord = $words[1];
-  if(!empty($secWord) && $secWord[0] == "L"){
-        return true;
-  } 
-}
-  return false;
-}
-
-function addArraysLec($a,$b){
-  $lec = [];
-  foreach($a as $k=>$v){
-    if(!empty($a[$k-1]) && !empty($v) && checkLec($a[$k-1])){
-      $lec[$k-1] = $a[$k-1];
-      if(!empty($b[$k])){
-              $lec[$k] = $v . "," .$b[$k]; 
-      }else{
-              $lec[$k] = $v; 
-      }
-    }else{
-      $lec[$k] = '';
-    }
-  }
-  return $lec;
-}
-
-function trimArray(&$array){
-  while (empty(end($array)))
+function getMagnificentKeys($arr)
 {
-    array_pop($array);
-}
-    while (empty($array[0]))
-{
-  unset($array[0]);
-}
-  
-}
+    $keys = [];
+    $prev = 0;
+    foreach ($arr as $k => $v) {
+        if (abs($prev - $v) > 7) {
+            // $keys[] = $k;
+            $keys[$k] = "break";
+        } else {
+            $prev = $v;
+            $keys[$k] = "batchdet";
 
-function arrayColumn($multiArray , $index){
-$columnIndex = $index;
-$columnWithIndexes = array();
-foreach ($multiArray as $key => $row) {
-    if (isset($row[$columnIndex])) {
-        $columnWithIndexes[$key] = $row[$columnIndex];
+        }
     }
-}
-  return $columnWithIndexes;
-}
-
-
-function giveCorrectTime($start,$end,$d){
-  $m = 50;
-  $firstWord = strtok($d, " ");
-  $letterAfterFirstWord = substr($d, strlen($firstWord) + 1, 1);
-  
-  if($letterAfterFirstWord == "P"){
-    $m = 100;
-  }
-  
-  $start_ = DateTime::createFromFormat('h:i A', $start);
-  $end_ = DateTime::createFromFormat('h:i A', $end);
-
-  if($end_ < $start_ && !empty($end)){
-    $end_ = $start_->add(new DateInterval('PT' . $m . 'M'));
-    $end = $start_->format('h:i A');
-  }
-  
-  return "$start - $end";
+    return $keys;
 }
 
 
-// function etgSchedule($timings, $schedule) {
-//   print_r([$timings,$schedule]);
-//       $days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' );
-//     $events = [];
-//     // $event = ["details": "" ,"time": ""];
-//     $cur_week_index = 0;  
-//     foreach($timings as $skey => $time){
-//        if(trim($time) == "06:50 PM"){
-//          $cur_week_index++;
-//        }
 
-//      $weekday = $days[$cur_week_index];
-//       $s = $schedule[$skey];
-//       if(!empty($s)){  
-//         $prevkey = array_key_last($events);
-//         if(str_contains($s,"U")){
-//           if(!empty($events)){
-          
-//           $events[$prevkey]["time"] = giveCorrectTime($events[$prevkey]["time"],$time,$events[$prevkey]["details"]);
-//               //end the prev event;array_key_last($arr)
-//           }
-//           $events[] = ["details"=> "$s" ,"time"=> "$time","week"=> $weekday];
-//         }else{
-//            $events[$prevkey]["details"] =  $events[$prevkey]["details"] . " " . $s;
-//         }          
-//       }
-  
-//     }
-//   return $events;
-// }
-
-function checkSubjectByRegex($str){
-  //function to check subject code..
-  $str = trim($str);
-  $subjectRegex = '/^[UP][A-Z0-9]{5}$/';
-  $subjectRegex = '/^[UP][A-Z0-9]+$/';
-  $words = explode(' ', $str);
-  $firstWord = $words[0];
-
-
-  if (preg_match($subjectRegex, $firstWord, $matches)) {
-   return true;
-  }
-  return false;
-  
-  // if(!empty($firstWord)){
-  //   $f = $firstWord[0];
-  //   if($f == $l){
-  //     return true;
-  //   }
-  // }
-  // return false;
+function addArrays($a, $b)
+{
+    foreach ($a as $k => $v) {
+        $vv = $b[$k];
+        if (!empty($v) && !empty($vv)) {
+            $a[$k] = $v . "," . $vv;
+        }
+    }
+    return $a;
 }
 
-function etgSchedule($timings, $schedule) {
-  // print_r([$timings,$schedule]);
-      $days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' );
+
+
+function checkLec($s)
+{
+    $sub = subjectReturner($s);
+    if ($sub) {
+        if ($sub[3] == "L") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function addArraysLec($a, $b)
+{
+    $lec = [];
+    foreach ($a as $k => $v) {
+        if (!empty($a[$k - 1]) && !empty($v) && checkLec($a[$k - 1])) {
+            $lec[$k - 1] = $a[$k - 1];
+            if (!empty($b[$k])) {
+                $lec[$k] = $v . "," . $b[$k];
+            } else {
+                $lec[$k] = $v;
+            }
+        } else {
+            $lec[$k] = '';
+        }
+    }
+    return $lec;
+}
+
+function trimArray(&$array)
+{
+    while (empty(end($array))) {
+        array_pop($array);
+    }
+    while (empty($array[0])) {
+        unset($array[0]);
+    }
+
+}
+
+function arrayColumn($multiArray, $index)
+{
+    $columnIndex = $index;
+    $columnWithIndexes = array();
+    foreach ($multiArray as $key => $row) {
+        if (isset($row[$columnIndex])) {
+            $columnWithIndexes[$key] = $row[$columnIndex];
+        }
+    }
+    return $columnWithIndexes;
+}
+
+
+
+function subjectReturner($subjectString)
+{
+    $bestSubRegex = '/[UP][A-Za-z]{2}\d{3}/m';
+
+    if (preg_match($bestSubRegex, $subjectString, $matches)) {
+        // $remainingString = str_replace($matches[0], '', $subjectString);
+        $remainingString = preg_replace($bestSubRegex, '', $subjectString);
+        $trimmedString = trim($remainingString);
+        $trimmedString = ltrim($trimmedString, '/');
+        $firstLetter = substr($trimmedString, 0, 1);
+
+        if (!empty($firstLetter)) {
+            if ($firstLetter == "L") {
+                return ["Lecture", trim($matches[0]), 1, $firstLetter];
+            } else if ($firstLetter == "T") {
+                return ["Tutorial", trim($matches[0]), 1, $firstLetter];
+            } else if ($firstLetter == "P") {
+                return ["Practical", trim($matches[0]), 2, $firstLetter];
+            } else if ($firstLetter == "E") {
+                return ["Elective", trim($matches[0]), 1, $firstLetter];
+            }
+        }
+    }
+    return false;
+
+}
+
+
+function etgSchedule($timings, $schedule)
+{
+    // print_r([$timings,$schedule]);
+    $days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
     $events = [];
     // $event = ["details": "" ,"time": ""];
-    $cur_week_index = 0;  
-    foreach($timings as $skey => $time){
-       if(trim($time) == "06:50 PM"){
-         $cur_week_index++;
-       }
-
-       $weekday = $days[$cur_week_index];
-       $s = $schedule[$skey];
-      if(!empty($s)){  
-        if(checkSubjectByRegex($s)){
-            $start = $time;
-            $start_ = DateTime::createFromFormat('h:i A', $start);
-            $firstWord = strtok($s, " ");
-            $letterAfterFirstWord = substr($s, strlen($firstWord) + 1, 1);
-            
-            $no_of_row_next = 1;
-            if($letterAfterFirstWord == "P"){
-              $no_of_row_next = 2;
-            }
-            
-            $m = $no_of_row_next * 50;
-            if(!empty($start_)){
-            $end_ = $start_->add(new DateInterval('PT' . $m . 'M'));
-            $end = $start_->format('h:i A');
-            $ftime = "$start - $end";
-            $ll = $no_of_row_next +  $skey;
-          
-             for ($i = $skey + 1; $i <= $ll; $i++) {
-              $s .=  ' ' . $schedule[$i];
-              }
-
-            $events[] = ["details"=> "$s" ,"time"=> "$ftime","week"=> $weekday];
-            }
-          
+    $cur_week_index = 0;
+    foreach ($timings as $skey => $time) {
+        $start = parseTime($time);
+        if($start == false) {
+            print_r($time);
+            continue;
         }
-      }
-  
+        $strStart = $start->format('h:i A');
+
+        if ($strStart == "06:50 PM") {
+            $cur_week_index++;
+        }   
+
+        $weekday = $days[$cur_week_index];
+        $s = $schedule[$skey];
+        if (!empty($s)) {
+            $isSubject = subjectReturner($s);
+            if (!empty($isSubject)) {
+                [$type, $code, $no_of_row_next, $firstLetter] = $isSubject;
+
+                $m = $no_of_row_next * 50;
+
+
+
+                if (!empty($strStart)) {
+                        $end = $start->add(new DateInterval('PT' . $m . 'M'));
+                        $strEnd = $end->format('h:i A');
+                        $fTime = "$strStart - $strEnd";
+                        $ll = $no_of_row_next + $skey;
+
+                        $extra = "";
+                        for ($i = $skey + 1; $i <= $ll; $i++) {
+                            $extra .= ' ' . $schedule[$i];
+                        }
+
+                        $events[] = ["details" => "$code $type $extra", "time" => $fTime, "week" => $weekday];
+                }
+
+            }
+        }
+
     }
-  return $events;
+    return $events;
 }
 
-function displayShedule($timings, $schedule) {
-  print_r(etgSchedule($timings, $schedule));
+function displayShedule($timings, $schedule)
+{
+    print_r(etgSchedule($timings, $schedule));
 }
 
+
+
+
+function parseTime($inputString) {
+    $inputString = trim($inputString);
+    $ampm = substr($inputString, -2);
+    $timePart = substr($inputString, 0, -2);
+    $timePart = trim($timePart);
+
+    $timeParts = preg_split('/[^0-9]+/', $timePart, -1, PREG_SPLIT_NO_EMPTY);
+    if (count($timeParts) >= 2) {
+        $hours = (int) $timeParts[0];
+        $minutes = (int) $timeParts[1];
+    } else {
+        $hours = $minutes = 0;
+    }
+    $dateTimeString = sprintf('%02d:%02d %s', $hours, $minutes, $ampm);
+    $dateTimeObject = DateTime::createFromFormat('h:i A', $dateTimeString);
+
+    return $dateTimeObject;
+}
 
 
 
--- temp.php
+++ temp.php
@@ -1,16 +1,41 @@
 <?php
 
+function parseTime($inputString) {
+    // Remove any leading/trailing whitespaces
+    $inputString = trim($inputString);
 
-$subject = "UES103";
-$regex = '/^[UP][A-Z0-9]{5}$/';
+    // Get AM/PM from the last two characters of the input string
+    $ampm = substr($inputString, -2);
 
-if (preg_match($regex, $subject, $matches)) {
-    // The first word of the string matches the regex
-    $firstWord = $matches[0];
-    echo "First word matched: " . $firstWord;
-} else {
-    // The first word of the string does not match the regex
-    echo "No match found for the first word.";
+    // Get the time part by removing the AM/PM from the input string
+    $timePart = substr($inputString, 0, -2);
+    $timePart = trim($timePart);
+
+    // Separate the hours and minutes by splitting at non-numeric characters
+    $timeParts = preg_split('/[^0-9]+/', $timePart, -1, PREG_SPLIT_NO_EMPTY);
+    if (count($timeParts) >= 2) {
+        $hours = (int) $timeParts[0];
+        $minutes = (int) $timeParts[1];
+    } else {
+        $hours = $minutes = 0;
+    }
+
+    // Create a DateTime object using the parsed values
+    $dateTimeString = sprintf('%02d:%02d %s', $hours, $minutes, $ampm);
+    $dateTimeObject = DateTime::createFromFormat('h:i A', $dateTimeString);
+
+    return $dateTimeObject;
 }
 
+
+// Test the function with sample input strings
+$strings = array("9:40: AM", "08:50:AM", "10:30 AM", "6.50 PM");
+
+foreach ($strings as $string) {
+    $timeInfo = parseTime($string);
+
+    // Display the extracted values
+    print_r($timeInfo);
+    echo "        ";
+}
   ?>
